52d7db72ead806621e2347a2f930e0da4aeb3540
Elektronenbindung <52044541+elektronenbindung@users.noreply.github.com>
2025-06-13 15:52:13 +0200

    Use eclipse formatting

--- src/main/java/scheduling/Main.java
+++ src/main/java/scheduling/Main.java
@@ -7,36 +7,37 @@ import scheduling.common.Config;
 
 public final class Main {
 
-  private Main() {}
-
-  public static void main(String[] args) {
-    if (args.length != 1) {
-      System.out.println(
-          "Hint: Provide exactly one single parameter on console as an input file - Using UI instead");
-      new UI(getVersion()).setVisible(true);
-    } else {
-      if (args[0].equals(Config.VERSION)) {
-        System.out.println("version: " + getVersion());
-        System.exit(0);
-      }
-      System.out.println("You can quit by typing '" + Config.QUIT + "'");
-      File input = new File(args[0]);
-      ThreadsController threadsController = new ThreadsController(input, null);
-      new Thread(threadsController).start();
-
-		//noinspection InfiniteLoopStatement
-		while (true) {
-        String line = System.console().readLine();
-
-        if (line.equals(Config.QUIT)) {
-          threadsController.stop();
-        }
-      }
+    private Main() {
     }
-  }
 
-  private static String getVersion() {
-    Package mainPackage = Main.class.getPackage();
-	  return mainPackage.getImplementationVersion();
-  }
+    public static void main(String[] args) {
+	if (args.length != 1) {
+	    System.out.println(
+		    "Hint: Provide exactly one single parameter on console as an input file - Using UI instead");
+	    new UI(getVersion()).setVisible(true);
+	} else {
+	    if (args[0].equals(Config.VERSION)) {
+		System.out.println("version: " + getVersion());
+		System.exit(0);
+	    }
+	    System.out.println("You can quit by typing '" + Config.QUIT + "'");
+	    File input = new File(args[0]);
+	    ThreadsController threadsController = new ThreadsController(input, null);
+	    new Thread(threadsController).start();
+
+	    // noinspection InfiniteLoopStatement
+	    while (true) {
+		String line = System.console().readLine();
+
+		if (line.equals(Config.QUIT)) {
+		    threadsController.stop();
+		}
+	    }
+	}
+    }
+
+    private static String getVersion() {
+	Package mainPackage = Main.class.getPackage();
+	return mainPackage.getImplementationVersion();
+    }
 }
