f94b97c2d61f9a5e543cbb7a40bcacddd5c8e1f5
Elektronenbindung <52044541+elektronenbindung@users.noreply.github.com>
2025-07-15 20:00:58 +0200

    Refactoring of UiApplication

--- src/main/java/scheduling/ui/UiApplication.java
+++ src/main/java/scheduling/ui/UiApplication.java
@@ -1,5 +1,6 @@
 package scheduling.ui;
 
+import java.io.IOException;
 import java.util.Objects;
 
 import javafx.application.Application;
@@ -11,16 +12,24 @@ import scheduling.AppVersion;
 
 public class UiApplication extends Application {
 
+    private static final String APP_TITLE_PREFIX = "Scheduling ";
+    private static final String FXML_FILE_NAME = "UI.fxml";
+
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) throws IOException {
         String version = AppVersion.getVersion();
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("UI.fxml")));
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(
+                getClass().getClassLoader().getResource(FXML_FILE_NAME),
+                "FXML file " + FXML_FILE_NAME + " not found. Ensure it's in the classpath."));
+
         Scene scene = new Scene(root);
+
         stage.setScene(scene);
         stage.setResizable(false);
         stage.centerOnScreen();
-        stage.setTitle("Scheduling " + version);
+        stage.setTitle(APP_TITLE_PREFIX + version);
+
         stage.show();
     }
-
 }
