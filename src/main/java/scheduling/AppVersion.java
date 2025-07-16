2be3a4df0997416b8882eab439bd928761599a7a
Elektronenbindung <52044541+elektronenbindung@users.noreply.github.com>
2025-07-16 12:34:31 +0200

    Refactoring of AppVersion

--- src/main/java/scheduling/AppVersion.java
+++ src/main/java/scheduling/AppVersion.java
@@ -1,9 +1,20 @@
 package scheduling;
 
-public class AppVersion {
+import java.util.Objects;
+
+public final class AppVersion {
+
+	private AppVersion() {
+	}
+
 	public static String getVersion() {
 		Package mainPackage = Main.class.getPackage();
-		String version = mainPackage.getImplementationVersion();
-		return version != null ? version : "dev";
+
+		String version = null;
+		if (mainPackage != null) {
+			version = mainPackage.getImplementationVersion();
+		}
+
+		return Objects.requireNonNullElse(version, "dev");
 	}
 }
