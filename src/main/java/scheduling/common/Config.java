25e10f86e1717156b27db214788e042f1aa82acf
Elektronenbindung <52044541+elektronenbindung@users.noreply.github.com>
2025-09-14 12:09:24 +0200

    Adjust config to get better solutions

--- src/main/java/scheduling/common/Config.java
+++ src/main/java/scheduling/common/Config.java
@@ -12,11 +12,11 @@ public class Config {
 	public static final int WEIGHT_FOR_NORMAL_DAY = 1;
 	public static final int WEIGHT_FOR_FIXED_DAY = 1000;
 	public static final int WEIGHT_FOR_FREE_DAY = 32;
-	public static final int MAX_RETRIES_OF_TABU_SEARCH = 30000;
+	public static final int MAX_RETRIES_OF_TABU_SEARCH = 40000;
 	public static final int TABU_SEARCH_NEIGHBORHOOD_SAMPLE_SIZE = 100;
 	public static final int LENGTH_OF_TABU_LIST = 15;
-	public static final int MAX_RETRIES_OF_SOLUTION = 2000;
-	public static final int LENGTH_OF_SOLUTION_LIST = 20;
+	public static final int MAX_RETRIES_OF_SOLUTION = 4000;
+	public static final int LENGTH_OF_SOLUTION_LIST = 15;
 	public static final int DEFAULT_MAX_LENGTH_OF_SHIFT = 31;
 	public static final int INTERVAL_FOR_ONE_DAY = 2;
 	public static final double PENALTY_FOR_FORBIDDEN_SHIFT = 10000;
